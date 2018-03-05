51d847b059f5153c2d8558af8c6d9f202ff18921
wangshuqin <shuqin.wang@daxuezhang.com>
2018-03-05 12:40:17 +0800

    首页选择武汉不分汉口和武昌

--- routes/controller.js
+++ routes/controller.js
@@ -89,13 +89,7 @@ exports.index = function (req, res, next) {
         },
     },function (err, result) {
         var resIp = data.area = returnData(result.getNowCity, 'getNowCity');//当前ip所在城市id
-        if (resIp == 14) {
-            var areaArr = [50,51];
-            data.nowIp = areaArr[Math.round(Math.random())];
-        }
-        else {
-            data.nowIp = resIp;
-        }
+        data.nowIp = resIp;
         data.iparea = iparea;
         data.xSlider = returnData(result.lunbo_list,'lunbo_list');
         data.xSlider2 = returnData(result.lunbo_list2,'lunbo_list2');
