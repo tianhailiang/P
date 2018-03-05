5d4f5e5f7123a32327b16798096913638f8b8e2a
wangshuqin <shuqin.wang@daxuezhang.com>
2018-03-05 10:36:37 +0800

    view_count

---
+++ public/assets/js/view_count.js
@@ -0,0 +1,31 @@
+function create_token(code){
+    var c=String.fromCharCode(code.charCodeAt(0)+code.length);
+    for(var i=1;i<code.length;i++){
+        c+=String.fromCharCode(code.charCodeAt(i)+code.charCodeAt(i-1));
+    }
+    return escape(c);
+}
+function article_count (token,catid,articleid,uuid) {
+    $.ajax({
+        url: ajaxUrlPrefix.nodeapi + '/cmsapi/article_count',
+        type:'GET',
+        data:{
+            catid : catid,
+            id :articleid,
+            uuid: uuid,
+            token:token
+        },
+        success:function(res){
+            if (res.code == 0) {
+                $('#detail_views_num').html(res.data.num);
+                $.cookie('uuid', res.data.uuid,{ path: "/",domain: '.jjlvip.cn'});
+            }
+        }
+    });
+}
+$(function(){
+    var catid = '100';
+    var articleid = $("#detail_id").val();
+    var uuid = $.cookie('uuid') ? $.cookie('uuid') : '';
+    article_count(create_token("apitokenjjl.cn2018"),catid,articleid,uuid);
+});
