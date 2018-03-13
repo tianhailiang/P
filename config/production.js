26a5bef60fabb7927fa1507bf42b03b88e7559cb
duxiaoli <1151036495@qq.com>
2018-03-13 11:26:38 +0800

    修改 底页

--- config/production.js
+++ config/production.js
@@ -237,7 +237,9 @@ var apis = {
   //get_so_article_list
   "get_so_article_list": shequ_prefix + 'search/search_article/',
   //根据ip 获取城市code码
-  "get_ip_geter":prefix + 'ip_geter'
+  "get_ip_geter":prefix + 'ip_geter',
+  //金色力量底页
+  "get_culture_detail":prefix + 'detail'
 };
 module.exports = {
   redisCache: redisCache,
