9bde642a6930b851f329d2f2c556eb54774b06c8
wangshuqin <shuqin.wang@daxuezhang.com>
2018-04-02 14:23:38 +0800

    svg yanzhengma

--- routes/controller.js
+++ routes/controller.js
@@ -12,6 +12,7 @@ var code = '1220000006'; // not found
 var comfunc = require('../common/common');
 var tokenfunc = require('./token.js');
 var helperfunc = require('../common/helper');
+var svgCaptcha = require("svg-captcha");
 function returnData(obj,urlName){
   if(obj.code==0){
     return obj.data;
@@ -3622,4 +3623,24 @@ exports.save_feedback = function(req,res,next){
           res.send(result); 
         }
     })
+};
+// 验证码
+function verifycodeGetController(req, res) {
+    // 验证码，对了有两个属性，text是字符，data是svg代码
+    var code = svgCaptcha.create({
+        // 翻转颜色
+        inverse: false,
+        // 字体大小
+        fontSize: 36,
+        // 噪声线条数
+        noise: 3,
+        // 宽度
+        width: 80,
+        // 高度
+        height: 30
+    });
+    // 保存到session,忽略大小写
+    req.session["randomcode"] = code.text.toLowerCase();
+    // 返回数据直接放入页面元素展示即可
+    res.send(code.data);
 }
