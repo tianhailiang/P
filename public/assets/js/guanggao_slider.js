47cd315e531b2b19d51c655eedd984ac0c911f46
huangrongfa <rongfa.huang@daxuezhang.com>
2018-04-25 00:10:34 +0800

    添加广告位轮播

---
+++ public/assets/js/guanggao_slider.js
@@ -0,0 +1,401 @@
+// console.log($('.swiper-container_11').data('slider'))
+if($('.swiper-container_11').data('slider') > 3){
+		var mySwiper11 = new Swiper('.swiper-container_11',{
+			autoplay : 5000,
+			loop : true,
+			mode : 'horizontal',
+			speed:1300,
+			slidesPerView : 3,
+			slidesPerGroup : 3,
+			autoplayDisableOnInteraction : false
+	})
+	$(".swiper-container_11").mouseenter(function(){
+		mySwiper11.stopAutoplay();
+	})
+	$(".swiper-container_11").mouseleave(function(){
+		mySwiper11.startAutoplay();
+	})
+}
+
+if($('.swiper-container_12').data('slider') > 3){
+		var mySwiper12 = new Swiper('.swiper-container_12',{
+			autoplay : 4000,
+			loop : true,
+			mode : 'horizontal',
+			speed:1000,
+			slidesPerView : 3,
+			slidesPerGroup : 3
+	})
+	$(".swiper-container_12").mouseenter(function(){
+		mySwiper12.stopAutoplay();
+	})
+	$(".swiper-container_12").mouseleave(function(){
+		mySwiper12.startAutoplay();
+	})
+}
+
+if($('.swiper-container_13').data('slider') > 3){
+	var mySwiper13 = new Swiper('.swiper-container_13',{
+		autoplay : 4000,
+		loop : true,
+		mode : 'horizontal',
+		speed:1000,
+		slidesPerView : 3,
+		slidesPerGroup : 3,
+	})
+	$(".swiper-container_13").mouseenter(function(){
+		mySwiper13.stopAutoplay();
+	})
+	$(".swiper-container_13").mouseleave(function(){
+		mySwiper13.startAutoplay();
+	})
+}
+if($('.swiper-container_14').data('slider') > 3){
+	var mySwiper14 = new Swiper('.swiper-container_14',{
+		autoplay : 4000,
+		loop : true,
+		mode : 'horizontal',
+		speed:1000,
+		slidesPerView : 3,
+		slidesPerGroup : 3,
+	})
+	$(".swiper-container_14").mouseenter(function(){
+		mySwiper14.stopAutoplay();
+	})
+	$(".swiper-container_14").mouseleave(function(){
+		mySwiper14.startAutoplay();
+	})
+}
+if($('.swiper-container_15').data('slider') > 3){
+	var mySwiper15 = new Swiper('.swiper-container_15',{
+		autoplay : 4000,
+		loop : true,
+		mode : 'horizontal',
+		speed:1000,
+		slidesPerView : 3,
+		slidesPerGroup : 3,
+})
+	$(".swiper-container_15").mouseenter(function(){
+		mySwiper15.stopAutoplay();
+	})
+	$(".swiper-container_15").mouseleave(function(){
+		mySwiper15.startAutoplay();
+	})
+}
+if($('.swiper-container_16').data('slider') > 3){
+	var mySwiper16 = new Swiper('.swiper-container_16',{
+		autoplay : 4000,
+		loop : true,
+		mode : 'horizontal',
+		speed:1000,
+		slidesPerView : 3,
+		slidesPerGroup : 3,
+	})
+	$(".swiper-container_16").mouseenter(function(){
+		mySwiper16.stopAutoplay();
+	})
+	$(".swiper-container_16").mouseleave(function(){
+		mySwiper16.startAutoplay();
+	})
+}
+if($('.swiper-container_17').data('slider') > 3){
+	var mySwiper17 = new Swiper('.swiper-container_17',{
+		autoplay : 4000,
+		loop : true,
+		mode : 'horizontal',
+		speed:1000,
+		slidesPerView : 3,
+		slidesPerGroup : 3,
+	})
+	$(".swiper-container_17").mouseenter(function(){
+		mySwiper17.stopAutoplay();
+	})
+	$(".swiper-container_17").mouseleave(function(){
+		mySwiper17.startAutoplay();
+	})
+}
+if($('.swiper-container_18').data('slider') > 3){
+	var mySwiper18 = new Swiper('.swiper-container_18',{
+		autoplay : 4000,
+		loop : true,
+		mode : 'horizontal',
+		speed:1000,
+		slidesPerView : 3,
+		slidesPerGroup : 3,
+	})
+	$(".swiper-container_18").mouseenter(function(){
+		mySwiper18.stopAutoplay();
+	})
+	$(".swiper-container_18").mouseleave(function(){
+		mySwiper18.startAutoplay();
+	})
+}
+if($('.swiper-container_19').data('slider') > 3){
+	var mySwiper19 = new Swiper('.swiper-container_19',{
+		autoplay : 4000,
+		loop : true,
+		mode : 'horizontal',
+		speed:1000,
+		slidesPerView : 3,
+		slidesPerGroup : 3,
+	})
+	$(".swiper-container_19").mouseenter(function(){
+		mySwiper19.stopAutoplay();
+	})
+	$(".swiper-container_19").mouseleave(function(){
+		mySwiper19.startAutoplay();
+	})
+}
+if($('.swiper-container_20').data('slider') > 3){
+	var mySwiper20 = new Swiper('.swiper-container_20',{
+		autoplay : 4000,
+		loop : true,
+		mode : 'horizontal',
+		speed:1000,
+		slidesPerView : 3,
+		slidesPerGroup : 3,
+	})
+	$(".swiper-container_20").mouseenter(function(){
+		mySwiper20.stopAutoplay();
+	})
+	$(".swiper-container_20").mouseleave(function(){
+		mySwiper20.startAutoplay();
+	})
+}
+if($('.swiper-container_21').data('slider') > 3){
+	var mySwiper21 = new Swiper('.swiper-container_21',{
+		autoplay : 4000,
+		loop : true,
+		mode : 'horizontal',
+		speed:1000,
+		slidesPerView : 3,
+		slidesPerGroup : 3,
+	})
+	$(".swiper-container_21").mouseenter(function(){
+		mySwiper21.stopAutoplay();
+	})
+	$(".swiper-container_21").mouseleave(function(){
+		mySwiper21.startAutoplay();
+	})
+}
+if($('.swiper-container_22').data('slider') > 3){
+	var mySwiper22 = new Swiper('.swiper-container_22',{
+		autoplay : 4000,
+		loop : true,
+		mode : 'horizontal',
+		speed:1000,
+		slidesPerView : 3,
+		slidesPerGroup : 3,
+	})
+	$(".swiper-container_22").mouseenter(function(){
+		mySwiper22.stopAutoplay();
+	})
+	$(".swiper-container_22").mouseleave(function(){
+		mySwiper22.startAutoplay();
+	})
+}
+if($('.swiper-container_23').data('slider') > 3){
+	var mySwiper23 = new Swiper('.swiper-container_23',{
+		autoplay : 4000,
+		loop : true,
+		mode : 'horizontal',
+		speed:1000,
+		slidesPerView : 3,
+		slidesPerGroup : 3,
+	})
+	$(".swiper-container_23").mouseenter(function(){
+		mySwiper23.stopAutoplay();
+	})
+	$(".swiper-container_23").mouseleave(function(){
+		mySwiper23.startAutoplay();
+	})
+}
+if($('.swiper-container_24').data('slider') > 3){
+	var mySwiper24 = new Swiper('.swiper-container_24',{
+		autoplay : 4000,
+		loop : true,
+		mode : 'horizontal',
+		speed:1000,
+		slidesPerView : 3,
+		slidesPerGroup : 3,
+	})
+	$(".swiper-container_24").mouseenter(function(){
+		mySwiper24.stopAutoplay();
+	})
+	$(".swiper-container_24").mouseleave(function(){
+		mySwiper24.startAutoplay();
+	})
+}
+
+if($('.swiper-container_25').data('slider') > 3){
+	var mySwiper25 = new Swiper('.swiper-container_25',{
+		autoplay : 4000,
+		loop : true,
+		mode : 'horizontal',
+		speed:1000,
+		slidesPerView : 3,
+		slidesPerGroup : 3,
+	})
+	$(".swiper-container_25").mouseenter(function(){
+		mySwiper25.stopAutoplay();
+	})
+	$(".swiper-container_25").mouseleave(function(){
+		mySwiper25.startAutoplay();
+	})
+}
+if($('.swiper-container_26').data('slider') > 3){
+	var mySwiper26 = new Swiper('.swiper-container_26',{
+		autoplay : 4000,
+		loop : true,
+		mode : 'horizontal',
+		speed:1000,
+		slidesPerView : 3,
+		slidesPerGroup : 3,
+	})
+	$(".swiper-container_26").mouseenter(function(){
+		mySwiper26.stopAutoplay();
+	})
+	$(".swiper-container_26").mouseleave(function(){
+		mySwiper26.startAutoplay();
+	})
+}
+if($('.swiper-container_27').data('slider') > 3){
+	var mySwiper27 = new Swiper('.swiper-container_27',{
+		autoplay : 4000,
+		loop : true,
+		mode : 'horizontal',
+		speed:1000,
+		slidesPerView : 3,
+		slidesPerGroup : 3,
+	})
+	$(".swiper-container_27").mouseenter(function(){
+		mySwiper27.stopAutoplay();
+	})
+	$(".swiper-container_27").mouseleave(function(){
+		mySwiper27.startAutoplay();
+	})
+}
+if($('.swiper-container_28').data('slider') > 3){
+	var mySwiper28 = new Swiper('.swiper-container_28',{
+		autoplay : 4000,
+		loop : true,
+		mode : 'horizontal',
+		speed:1000,
+		slidesPerView : 3,
+		slidesPerGroup : 3,
+	})
+	$(".swiper-container_28").mouseenter(function(){
+		mySwiper28.stopAutoplay();
+	})
+	$(".swiper-container_28").mouseleave(function(){
+		mySwiper28.startAutoplay();
+	})
+}
+if($('.swiper-container_29').data('slider') > 3){
+	var mySwiper29 = new Swiper('.swiper-container_29',{
+		autoplay : 4000,
+		loop : true,
+		mode : 'horizontal',
+		speed:1000,
+		slidesPerView : 3,
+		slidesPerGroup : 3,
+	})
+	$(".swiper-container_29").mouseenter(function(){
+		mySwiper29.stopAutoplay();
+	})
+	$(".swiper-container_29").mouseleave(function(){
+		mySwiper29.startAutoplay();
+	})
+}
+if($('.swiper-container_30').data('slider') > 3){
+	var mySwiper30 = new Swiper('.swiper-container_30',{
+		autoplay : 4000,
+		loop : true,
+		mode : 'horizontal',
+		speed:1000,
+		slidesPerView : 3,
+		slidesPerGroup : 3,
+	})
+	$(".swiper-container_30").mouseenter(function(){
+		mySwiper30.stopAutoplay();
+	})
+	$(".swiper-container_30").mouseleave(function(){
+		mySwiper30.startAutoplay();
+	})
+}
+if($('.swiper-container_31').data('slider') > 3){
+	var mySwiper31 = new Swiper('.swiper-container_31',{
+		autoplay : 4000,
+		loop : true,
+		mode : 'horizontal',
+		speed:1000,
+		slidesPerView : 3,
+		slidesPerGroup : 3,
+	})
+	$(".swiper-container_31").mouseenter(function(){
+		mySwiper31.stopAutoplay();
+	})
+	$(".swiper-container_31").mouseleave(function(){
+		mySwiper31.startAutoplay();
+	})
+	// $('.arrow-left').on('click', function(e){
+	// 	e.preventDefault()
+	// 	mySwiper31.swipePrev()
+	//  })
+	//  $('.arrow-right').on('click', function(e){
+	// 	e.preventDefault()
+	// 	mySwiper31.swipeNext()
+	//  })
+}
+if($('.swiper-container_32').data('slider') > 3){
+	var mySwiper32 = new Swiper('.swiper-container_32',{
+		autoplay : 4000,
+		loop : true,
+		mode : 'horizontal',
+		speed:1000,
+		slidesPerView : 3,
+		slidesPerGroup : 3,
+	})
+	$(".swiper-container_32").mouseenter(function(){
+		mySwiper32.stopAutoplay();
+	})
+	$(".swiper-container_32").mouseleave(function(){
+		mySwiper32.startAutoplay();
+	})
+}
+if($('.swiper-container_33').data('slider') > 3){
+	var mySwiper33 = new Swiper('.swiper-container_33',{
+		autoplay : 4000,
+		loop : true,
+		mode : 'horizontal',
+		speed:1000,
+		slidesPerView : 3,
+		slidesPerGroup : 3,
+	})
+	$(".swiper-container_33").mouseenter(function(){
+		mySwiper33.stopAutoplay();
+	})
+	$(".swiper-container_33").mouseleave(function(){
+		mySwiper33.startAutoplay();
+	})
+}
+if($('.swiper-container_34').data('slider') > 3){
+	var mySwiper34 = new Swiper('.swiper-container_34',{
+		autoplay : 4000,
+		loop : true,
+		mode : 'horizontal',
+		speed:1000,
+		slidesPerView : 3,
+		slidesPerGroup : 3,
+	})
+	$(".swiper-container_34").mouseenter(function(){
+		mySwiper34.stopAutoplay();
+	})
+	$(".swiper-container_34").mouseleave(function(){
+		mySwiper34.startAutoplay();
+	})
+}
+		
+
+
+
