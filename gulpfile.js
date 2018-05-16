ad66bf5565293d2ed45fb9be14b74b168cabab9f
wangshuqin <shuqin.wang@daxuezhang.com>
2018-05-16 15:06:47 +0800

    rename bootstrap nodemain.css

--- gulpfile.js
+++ gulpfile.js
@@ -54,7 +54,7 @@ gulp.task('minifycss', function() {
       compatibility: 'ie7',//保留ie7及以下兼容写法 类型：String 默认：''or'*' [启用兼容模式； 'ie7'：IE7兼容模式，'ie8'：IE8兼容模式，'*'：IE9+兼容模式]
       keepBreaks: true//类型：Boolean 默认：false [是否保留换行]
     }))
-    .pipe(concat('nodemain2.min.css'))
+    .pipe(concat('nodemainnew.min.css'))
     //.pipe(rev())
     .pipe(gulp.dest('public/assets/css')) //输出文件夹
     .pipe(notify({message: 'css压缩执行成功'}));
