225c23a5a37dbaf0b5e68d4673e762999d716721
wangweijiu <Weijiu.Wang@daxuezhang.com>
2018-08-10 18:10:35 +0800

    修改评论点赞接口 审核人：王淑琴

--- config/development.js
+++ config/development.js
@@ -39,7 +39,7 @@ var apis = {
   "yimin_xiangguanguwen": shequ_prefix + 'article/immi_related_adviser/', // 社区 相关顾问
   "xiugai_password": uc_prefix + 'index.php?m=editpassword',//社区 修改密码
   "xiugai_info": so_prefix + 'account/edit_user_info', //社区 修改用户信息
-  "favorite_article": shequ_prefix + 'article/favorite_article/',//社区 文章收藏
+  "favorite_article": so_prefix + 'article/favorite_article',//社区 文章收藏
   "article_vote": shequ_prefix + 'article/article_vote/',//社区 文章点赞
   "user_article_list": shequ_prefix + 'account/user_article_list/',//社区 专栏列表
   "post_login": uc_prefix + 'index.php?m=adviserLogin',//顾问登录
