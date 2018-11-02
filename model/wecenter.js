3bed062eb3f078cdb66e89dc0f812d54f0280ed7
wangweijiu <Weijiu.Wang@daxuezhang.com>
2018-11-02 13:59:18 +0800

    修改顾问中心主页

--- model/wecenter.js
+++ model/wecenter.js
@@ -453,3 +453,13 @@ exports.top_adviser_list = function (data,callback){
   api.apiRequest(url, callback);
   console.log('url-----', url);
 }
+// 顾问个人主页文章统计
+exports.gwzs = function (data,callback){
+  var url = _api_url_path(data, config.apis.article_count);
+  if (url == null) {
+    callback('404');
+    return;
+  }
+  api.apiRequest(url, callback);
+  console.log('url-----', url);
+}
