229919faaa2f2567339534f479b00216dd8bac41
wangshuqin <shuqin.wang@daxuezhang.com>
2018-11-14 18:15:24 +0800

    文章底页点赞数

--- public/assets/js/avatarGroup.js
+++ public/assets/js/avatarGroup.js
@@ -11,7 +11,7 @@ $(function(){
         success:function(msg){
             if (msg.code == 0 && msg.data.length>0) {
                 var avatarArr = msg.data;
-                $('#avatar-block').html(avatarGroup(avatarArr));
+                $('#fans-block').html(avatarGroup(avatarArr));
             }
         },
         error:function(XMLHttpRequest, textStatus, errorThrown){
@@ -20,10 +20,10 @@ $(function(){
     })
 })
 function avatarGroup (arr) {
-    var html = '';
+    var html = '<div class="avatar-block">';
     var fansNum = arr.length;
     for (var i = 0; i<fansNum;i++) {
         html+='<img src="'+ arr[i].avatar_file +'" alt="" style="margin-left:-8px;">';
     }
-    return html+'<span style="padding-left:2px;">等'+ fansNum +'人点赞</span>';
+    return html+'<span style="padding-left:2px;">等'+ fansNum +'人点赞</span></div>';
 }
