7b5f8dc3f727266a785edcf1b81ea9096ea1e681
wangshuqin <shuqin.wang@daxuezhang.com>
2018-11-14 17:59:36 +0800

    文章底页点赞数

--- public/assets/js/avatarGroup.js
+++ public/assets/js/avatarGroup.js
@@ -1,24 +1,29 @@
+console.log('avatarGroup.js')
+$(function(){
+    var articleid = $("#detail_id").val();
+    $.ajax({
+        url: ajaxUrlPrefix.nodeapi+'/vote_list',
+        data: {
+            article_id: articleid
+        },
+        type: 'GET',
+        dataType:'json',
+        success:function(msg){
+            if (msg.code == 0 && msg.data.length>0) {
+                var avatarArr = msg.data;
+                $('#avatar-block').html(avatarGroup(avatarArr));
+            }
+        },
+        error:function(XMLHttpRequest, textStatus, errorThrown){
+            console.log("获取失败，请重试！CODE:"+XMLHttpRequest.status)
+        }
+    })
+})
 function avatarGroup (arr) {
     var html = '';
     var fansNum = arr.length;
-    var fansInfoText = '<span>等'+ fansNum +'人点赞</span>'
     for (var i = 0; i<fansNum;i++) {
-        html+='<img src="http://images.jjl.cn/avatar/000/00/02/27_avatar_small_1521010157.jpg" alt="" style="margin-left:-8px;">';
+        html+='<img src="'+ arr[i].avatar_file +'" alt="" style="margin-left:-8px;">';
     }
-    return html+'等'+ fansNum +'人点赞';
+    return html+'<span style="padding-left:2px;">等'+ fansNum +'人点赞</span>';
 }
-var avatarArr=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30];
-$.ajax({
-    url: 'http://192.168.202.171:8080/so/api/article/vote_list',
-    data: {
-        article_id: 15412
-    },
-    type: 'GET',
-    success:function(msg){
-        console.log(msg);
-      },
-      error:function(XMLHttpRequest, textStatus, errorThrown){
-        console.log("获取失败，请重试！CODE:"+XMLHttpRequest.status)
-      }
-})
-$('#avatar-block').html(avatarGroup(avatarArr));
