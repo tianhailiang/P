c4851535702c2f4c1fb0a126a021e24f5e1ea7f4
thl <HaiLiang.Tian@daxuezhang.com>
2018-11-27 15:57:20 +0800

    修改字段

--- public/dep/js/schooltopic/Newform123.js
+++ public/dep/js/schooltopic/Newform123.js
@@ -92,3 +92,15 @@ function checkInput (value) {
 	var reg = /^[A-Za-z0-9_\u4e00-\u9fa5]{1,20}$/;
 	return reg.test(value);
 }
+//获取cookie
+function getCookie(cname) {
+	var name = cname + "=";
+	var ca = document.cookie.split(';');
+	for(var i=0; i<ca.length; i++) {
+	 var c = ca[i];
+	 while (c.charAt(0)==' ') c = c.substring(1);
+	 if (c.indexOf(name) != -1) return c.substring(name.length, c.length);
+	}
+	//如果没有获取到城市cookie，默认取1，表示取北京
+	return "1";
+ }
