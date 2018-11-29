821368a72fdc89873b58af0617fe6d57886ff1b3
thl <HaiLiang.Tian@daxuezhang.com>
2018-11-29 15:16:57 +0800

    ..

--- public/assets/js/common/country_list.js
+++ public/assets/js/common/country_list.js
@@ -241,18 +241,45 @@ function screenTag (country,...eduList) {
   //根据国家 学历参数筛选标签
   console.log('country ',country)
   console.log('eduList ',eduList)
-  if (country == 0) {
-    for (let item of tag_list) {
-      console.log(item.tagName)
+  if (country == 0 && eduList[0] == 0) {
+    //默认国家全部 学历全部 循环所有标签
+    for (let eItem of edu_list) {
+      console.log('全部学历 ',eItem.eduName)
     }
-  } else {
-    for (let item of tag_list) {
-      if(item.country.includes(country)) {
-        // console.log('下一步循环')
+    for (let tItem of tag_list) {
+      console.log('全部标签 ',tItem.tagName)
+    }
+  } else if(country == 0 && eduList[0] != 0) {
+    //在国家没选择情况下 勾选学历
+    console.log('请先选择国家')
+    return false;
+  } else if (country !=0 && eduList[0] == 0) {
+    //在选择国家时 变换学历 这时默认学历为 0
+    let edu_id_List = [];
+    for (let eItem of edu_list) {
+      if (eItem.country.includes(country)) {
+        console.log('选择国家时学历 ',eItem.eduName)
+        edu_id_List.push(eItem.eduId)
+      }
+    }
+    console.log('选择国家时学历数组ID ',edu_id_List)
+    for (let tItem of tag_list) {
+      if(tItem.country.includes(country)) {
+        for (let eduId of edu_id_List) {
+          if (tItem.edu.includes(eduId)) {
+            console.log('选择国家时标签 '+tItem.tagId,tItem.tagName)
+            break;
+          }
+        }
+      }
+    }
+  } else if (country !=0 && eduList[0] != 0) {
+    //点击学历时 变换所有标签
+    for (let tItem of tag_list) {
+      if(tItem.country.includes(country)) {
         for (let eduId of eduList) {
-          // console.log('eduId ',eduId)
-          if (item.edu.includes(eduId)) {
-            console.log(item.tagName)
+          if (tItem.edu.includes(eduId)) {
+            console.log('点击学历时出所有标签 '+tItem.tagId,tItem.tagName)
             break;
           }
         }
@@ -260,19 +287,3 @@ function screenTag (country,...eduList) {
     }
   }
 }
-function screenEdu (country) {
-  if (country == 0) {
-    for (let item of edu_list) {
-      console.log(item.eduName)
-    }
-  } else {
-    for (let item of edu_list) {
-      if (item.country.includes(country)) {
-        console.log(item.eduName)
-      }
-    }
-  }
-}
-// screenTag(1,1,2,3)
-screenEdu(0)
-screenEdu(100)
