a8242934aa541b6f44657260d0cdc5e262142b4f
wangweijiu <weijiu.wang@dxzjjl.com>
2019-03-11 14:51:01 +0800

    修改登录

--- routes/login.js
+++ routes/login.js
@@ -55,7 +55,7 @@ exports.loginUser = function (req, res, next) {
   var l = req.query
   console.log('url', l.h);
   if (/\/zt\//.test(l.h)) {
-    if (!l.h.endsWith('shtml')) {
+    if (l.h.endsWith('html')) {
       l.h.substring(l.h.length-10)
     }
   }
